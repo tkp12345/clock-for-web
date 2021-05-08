fe603a98da76e1f6df237a5ebc905dc1d39c60a9
tkp12345 <tkp1@hanmail.net>
2021-05-08 17:46:42 +0900

    todolist-start

---
+++ todo.js
@@ -0,0 +1,42 @@
+    const toDoForm = document.querySelector(".js-toDoForm"),
+        toDoInput= toDoForm.querySelector("input"),
+        toDoList = document.querySelector(".js-toDoList");
+
+    //상수
+    const TODOS_LS = 'toDos';
+
+    function paintToDo(text){
+        const li = document.createElement("li")
+        const delBtn = document.createElement("button");
+        delBtn.innerHTML ="🗑";
+        const span = document.createElement("span");
+        span.innerText = text; 
+        li.appendChild(span);
+        li.appendChild(delBtn);
+        toDoList.appendChild(li);
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        const currentValue = toDoInput.value;
+        paintToDo(currentValue);
+        toDoInput.value = "";
+    }
+
+    function loadToDos(){
+        const toDos = localStorage.getItem(TODOS_LS);
+          if(toDos !== null){
+
+
+
+            }else {
+
+            }
+}
+
+function init(){
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit)
+}
+
+init();
